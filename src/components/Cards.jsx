ed72d21ea69fd6e28e572d9d8a964801c991d9e7
Reza <149424191+Cosmodocus@users.noreply.github.com>
2024-03-12 21:15:40 -0400

    Add files via upload

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,7 +10,7 @@ const Cards = () => {
             <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
                 <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-                <p className='text-center text-4xl font-bold'>$149</p>
+                <p className='text-center text-5xl font-bold'>$149</p>
                 <div className='text-center font-medium'>
                     <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
                     <p className='py-2 border-b mx-8'>1 Granted User</p>
@@ -21,7 +21,7 @@ const Cards = () => {
             <div className="w-full shadow-xl flex flex-col p-4 my-8 md:m-0 rounded-lg hover:scale-105 duration-300 bg-transparent">
                 <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Double} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8'>Partnership</h2>
-                <p className='text-center text-4xl font-bold'>$199</p>
+                <p className='text-center text-5xl font-bold'>$199</p>
                 <div className='text-center font-medium'>
                     <p className='py-2 border-b mx-8 mt-8'>1 TB Storage</p>
                     <p className='py-2 border-b mx-8'>3 Users Allowed</p>
@@ -32,7 +32,7 @@ const Cards = () => {
             <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
                 <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
                 <h2 className='text-2xl font-bold text-center py-8'>Group Account</h2>
-                <p className='text-center text-4xl font-bold'>$299</p>
+                <p className='text-center text-5xl font-bold'>$299</p>
                 <div className='text-center font-medium'>
                     <p className='py-2 border-b mx-8 mt-8'>5 TB Storage</p>
                     <p className='py-2 border-b mx-8'>1 User Allowed</p>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
     <div className='text-white'>
         <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
             <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold '>Grow with Data.</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:text-5xl sm:text-4xl text-xl font-bold py-3'>Fast, flexible financing for</p>
                 <ReactTyped
                  className='md:text-5xl sm:text-4xl text-xl font-bold pl-2 text-[#00df9a] md:pl-4'
-                 strings={['BTB','BTC', 'SASS']} typeSpeed={120} 
-                 backSpeed={140} 
+                 strings={['BTB','SASS', 'BTC']} typeSpeed={100} 
+                 backSpeed={120} 
                  loop 
                 />
             </div>
